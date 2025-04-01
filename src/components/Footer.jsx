0f9c3eee21144d61648d657b9ed90b81dade64fe
xwaynex <nobodysshinigami@proton.me>
2025-04-01 08:50:02 +0100

    footer space

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 export default function Footer() {
   return (
-    <footer className='pt-20 pb-10'>
-      <small className="pt-5 border-t border-secondary opacity-50 justify-center flex text-xs">&copy; Copyright{ new Date(). getFullYear()} CitrusWheels. All rights reserved</small>
+    <footer className="pt-20 pb-10">
+      <small className="pt-5 border-t border-secondary opacity-50 justify-center flex text-xs">
+        &copy;{new Date().getFullYear()}&nbsp;CitrusWheels&nbsp;.All&nbsp;rights&nbsp;reserved
+      </small>
     </footer>
-  )
+  );
 }
